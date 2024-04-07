229a0f7807cfc04645a82e4b0fed40a275d69cb6
MA20014 <145225394+MA20014@users.noreply.github.com>
2024-04-07 02:16:00 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,18 +8,32 @@ int main() {
     Complex y(4.3, 8.2);
     Complex z(3.3, 1.1);
 
-    cout << "x: " << x << endl;
-    cout << "y: " << y << endl;
-    cout << "z: " << z << endl;
+    cout << "x: ";
+    x.print();
+    cout << endl;
+
+    cout << "y: ";
+    y.print();
+    cout << endl;
+
+    cout << "z: ";
+    z.print();
+    cout << endl;
 
     x = y + z;
-    cout << "\nx = y + z: " << x << endl;
+    cout << "\nx = y + z: ";
+    x.print();
+    cout << endl;
 
     x = y - z;
-    cout << "x = y - z: " << x << endl;
+    cout << "x = y - z: ";
+    x.print();
+    cout << endl;
 
     x = y * z;
-    cout << "x = y * z: " << x << endl;
+    cout << "x = y * z: ";
+    x.print();
+    cout << endl;
 
     return 0;
 }
