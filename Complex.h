4e44503f83b6525d3d6c1f05b9fbb8bb3de468bd
MA20014 <145225394+MA20014@users.noreply.github.com>
2024-04-07 02:16:56 -0700

    Update Complex.h

--- Complex.h
+++ Complex.h
@@ -5,9 +5,6 @@
 #include <iostream>
 
 class Complex {
-    friend std::ostream& operator<<(std::ostream&, const Complex&);
-    friend std::istream& operator>>(std::istream&, Complex&);
-
 public:
     explicit Complex(double = 0.0, double = 0.0); // constructor
     Complex operator+(const Complex&) const; // addition
@@ -15,6 +12,7 @@ public:
     Complex operator*(const Complex&) const; // multiplication
     bool operator==(const Complex&) const; // equality
     bool operator!=(const Complex&) const; // inequality
+    void print() const; // output
 
 private:
     double real; // real part
